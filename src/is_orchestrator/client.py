a74565716fbc5f1f3fb1e89ab5e929f0e76cc64c
wagnercotta <waacotta@gmail.com>
2020-01-06 17:18:13 -0300

    latest version

--- src/is_orchestrator/client.py
+++ src/is_orchestrator/client.py
@@ -11,15 +11,15 @@ import sys
 import os
 import time
 
-def has_detection(logger, service_name):
+def get_metric(logger, service_name):
     prometheus_address = "10.10.2.3:30900"
     url = "http://{}/api/v1/query?query={}".format(prometheus_address,service_name)
     data = requests.get(url).json()['data']['result']
     detect_counter = 0
     for elem in data:
         detect_counter += float(elem['value'][1])/len(data)
-    logger.info("Incerteza:{}".format(detect_counter))
-    return True if detect_counter >= 1.0 else False
+    # logger.info("Uncertainty:{}".format(detect_counter))
+    return detect_counter
 
 def main():
 
@@ -32,20 +32,25 @@ def main():
     broker_uri = config['broker_uri']
     channel = Channel(broker_uri)
     subscription = Subscription(channel)
+    with open("result-02-20-12-2019.txt", "a") as myfile:
+        range_cam_services = 4
+        for i in range(1,13):
+            logger.info("Changing FPS")
+            msg_config = CameraConfig()
+            msg_config.sampling.frequency.value = i
+            for num_svc in range(range_cam_services):
+                channel.publish(Message(content=msg_config, reply_to=subscription),topic="CameraGateway.{}.SetConfig".format(num_svc))
+                try:
+                    reply = channel.consume(timeout=1.0)
+                    logger.info('RPC Status: {}',reply.status)
+                except socket.timeout: 
+                    logger.info('No reply :(')
+            for x in range(3):
+                time.sleep(5)
+                metric = get_metric(logger,"uncertainty")
+                logger.info("FPS: {} - Uncertainty:{}",i,metric)
+                myfile.write("{},{}\n".format(i,metric))
 
-    range_cam_services = 4
-    for i in range(1,12):
-        logger.info("FPS = {}",i)
-        msg_config = CameraConfig()
-        msg_config.sampling.frequency.value = i
-        for num_svc in range(range_cam_services):
-            channel.publish(Message(content=msg_config, reply_to=subscription),topic="CameraGateway.{}.SetConfig".format(num_svc))
-            try:
-                reply = channel.consume(timeout=1.0)
-                logger.info('RPC Status: {}',reply.status)
-            except socket.timeout: 
-                logger.info('No reply :(')
-        time.sleep(2.5)
 
 if __name__ == '__main__':
     main()
